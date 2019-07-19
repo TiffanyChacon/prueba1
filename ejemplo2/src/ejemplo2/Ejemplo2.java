20c1d50a99c85c0da3852f79065737b290ed2055
Tiffany <>
2019-07-18 21:28:38 -0600

    Ejemplo 2 declarar variables, ingresar datos, mostrarlos

---
+++ ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre="", direccion="",telefono="";
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingresar Nombre");
+        nombre = leer.nextLine();
+        
+        System.out.println("Ingresar Direccion");
+        direccion = leer.nextLine();
+        
+        System.out.println("Ingresar Telefono");
+        telefono = leer.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\nSu direccion es: "+direccion+"\nSu Telefono es: "+telefono);
+    }
+    
+}
